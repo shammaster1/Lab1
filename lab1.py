4528cc1dc8cb8f2f530ab300e2a657a5a9624d29
shammaster1 <sham01@hotmail.ca>
2023-09-18 18:34:13 -0600

    final part

--- lab1.py
+++ lab1.py
@@ -2,3 +2,4 @@ import requests
 
 print(requests.__version__)
 print(requests.get("http://www.google.com/"))
+print("\n" + requests.get("https://raw.githubusercontent.com/shammaster1/Lab1/main/lab1.py").text)
